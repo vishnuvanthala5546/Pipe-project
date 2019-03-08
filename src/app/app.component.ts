53e9adfe91606a1bd724f669094ad22a50a258e7
Vishnupatel <vcvanthala.5546@gmail.com>
2019-03-08 09:15:57 +0530

    222 Using Pipes & Uppercase New started project
    new started pro uppercase

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Angular project';
+
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(2015,1,17)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(2016,3,23)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(2017,5,12)
+    },
+    {
+      instanceType: 'medium',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(2018,3,15)
+    }
+  ];
+
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}){
+      return{
+        'list-group-item-success': server.status === 'stable',
+        'list-group-item-warning': server.status === 'offline',
+        'list-group-item-danger': server.status === 'critical'
+      };
+  }
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
+
