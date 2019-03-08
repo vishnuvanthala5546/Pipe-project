4761817409f722d9cebb8696ca494ba2c2315fe2
Vishnupatel <vcvanthala.5546@gmail.com>
2019-03-08 10:23:54 +0530

    226 Creating a Custom Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {ShortenPipe} from './shorten.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
